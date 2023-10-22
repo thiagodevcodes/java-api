afcbb52e325bf6bef8f896b63d06cb2fdd0dd727
thiagodevcodes <euthiagosrodrigues@gmail.com>
2023-10-22 04:07:59 -0300

    Add all Acao classes

---
+++ src/main/java/br/com/thiago/orcamento/rest/form/AcaoUpdateForm.java
@@ -0,0 +1,27 @@
+package br.com.thiago.orcamento.rest.form;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+@Data
+public class AcaoUpdateForm {
+        @NotEmpty
+        @NotBlank
+        @Size(max = 100)
+        private String nome;
+
+        private Integer codigo;
+
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        private LocalDateTime dataCadastro;
+
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        private LocalDateTime dataAlteracao;
+
+}
